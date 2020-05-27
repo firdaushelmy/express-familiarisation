1f2f55f2a1614a7f83aa2d5c70b746fcb2ce4979
Firdaus Helmy <46524543+firdaushelmy@users.noreply.github.com>
2020-05-28 00:04:06 +0800

    sandbox

--- server.js
+++ server.js
@@ -3,6 +3,14 @@
 const express = require("express");
 const app = express();
 
+app.get("/", function (req, res) {
+  res.send("<h1>meow meow</h1>")
+});
+
+app.get("/about", function (req, res) {
+  res.send("<h2>I own this page mishzz</h2>")
+});
+
 app.listen(3000, function () {
   console.log("Server has started on port 3000")
 })
