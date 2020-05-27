8321dd6f98bb0d7d6b3d574757a0bbb660ce1308
Firdaus Helmy <46524543+firdaushelmy@users.noreply.github.com>
2020-05-28 00:16:45 +0800

    tried out nodemon

--- server.js
+++ server.js
@@ -7,8 +7,16 @@ app.get("/", function (req, res) {
   res.send("<h1>meow meow</h1>")
 });
 
+app.get("/contact", function (req, res) {
+  res.send("<h1>send me an email at meow@meow.com</h1>")
+});
+
 app.get("/about", function (req, res) {
   res.send("<h2>I own this page mishzz</h2>")
+});
+
+app.get("/hobbies", function (req, res) {
+  res.send("<ul><li>swimswim</li><li>beer</li><li>sleeping</li></ul>")
 });
 
 app.listen(3000, function () {
